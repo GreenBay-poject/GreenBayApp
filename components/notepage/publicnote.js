f1497cc204b4aab7fd4c2589116888fffc072d5e
Mohomed Haris Mohomed Ashkar <54017081+mashkarharis@users.noreply.github.com>
2021-09-30 18:19:08 +0530

    Fetch Notes Added

--- components/notepage/publicnote.js
+++ components/notepage/publicnote.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, Button, Dimensions, PixelRatio } from 'react-native';
 import { SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import PostCard from '../../atoms/post';
 import { requestPublicNotes } from '../../server/noterequestgenerator';
 
@@ -26,7 +26,14 @@ export default function PublicNote({user}) {
                 user['notes'].forEach(note=>{
                     note['email']=user['email'],
                     note['ministry_name']=user['ministry_name']
-                    notes_fetched.push(note)
+                    notes_fetched.push(
+                        <Marker
+                            key={note['note_id']}
+                            coordinate={{ latitude : note['lat'] , longitude : note['lon'] }}
+                            title={note['ministry_name']}
+                            description={note['text']}
+                        />
+                    )
                 })                
             });
             console.debug(notes_fetched)
@@ -55,14 +62,17 @@ export default function PublicNote({user}) {
                 size="large"
                 />
             :
-            <MapView
-                    initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
-            />
+            <MapView 
+                style={styles.mapview}
+                initialRegion={{
+                    latitude: 7.80,
+                    longitude: 80.50,
+                    latitudeDelta:2,
+                    longitudeDelta: 2,
+                }}
+            >
+                {notes}
+            </MapView>
 
         
         }
@@ -72,5 +82,9 @@ export default function PublicNote({user}) {
 }
 
 const styles = StyleSheet.create({
+    mapview:{
+        width:Dimensions.get('window').width,
+        height:Dimensions.get('window').height
+    }
 
 });
